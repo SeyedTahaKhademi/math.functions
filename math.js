97e2efd97ba1e7b84164e8d49cd24970cffbf8a8
Seyed Taha Khademi <thisistahakhademi@gmail.com>
2025-02-02 11:48:15 +0330

    Update math.js

--- math.js
+++ math.js
@@ -1,1 +1,42 @@
 
+function jam(item1 , item2) {
+
+    const a = item1;
+    const b = item2;
+    const c = a+b;
+    
+console.log(c);
+    
+};
+function menha(item1 , item2) {
+
+    const a = item1;
+    const b = item2;
+    const c = a-b;
+    
+console.log(c);
+    
+};
+function zarb(item1 , item2) {
+
+    const a = item1;
+    const b = item2;
+    const c = a*b;
+    
+console.log(c);
+    
+};
+function taghsim(item1 , item2) {
+
+    const a = item1;
+    const b = item2;
+    const c = a/b;
+    
+console.log(c);
+    
+};
+module.exports.plus = jam;
+module.exports.mines = menha;
+module.exports.zarb = zarb;
+module.exports.taghsim = taghsim;
+//this is exports for node js
